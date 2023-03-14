58eea5350543e3634f19375b9f5b279359a06ff8
Gianmarco Incocciati <minusgian@gmail.com>
2023-03-14 17:03:03 +0100

    add const btnLeft & const btnRight

--- js/script.js
+++ js/script.js
@@ -6,16 +6,25 @@ const images = [
     'img/01.jpg',
     'img/02.jpg',
     'img/03.jpg'
-]
+];
+
+let image_1 = images[0];
+let image_2 = images[1];
+let image_3 = images[2];
 
  for (let a = 0; a < images.length; a++) {
-    slides += `
+    slides = `
     <div class="slide w-100 h-100">
-     <img id="first_img" src="./img/01.jpg" alt="first_img_carousel">
-     <img class="d-none" src="./img/02.jpg" alt="second_img_carousel">
-     <img class="d-none" src="./img/03.jpg" alt="third_img_carousel">
+    <img id="first_img" src="${image_1}" alt="first_img_carousel">
+    <img src="${image_2}" alt="second_img_carousel">
+    <img src="${image_3}" alt="third_img_carousel">
     </div>
     `
- }
+ };
 
  slider.innerHTML += slides;
+
+ const btnLeft = document.getElementById('btnLeft');
+ const btnRight = document.getElementById('btnRight');
+
+let currentSlide = 
